0781bcc8271833366edb451ad7f9d6cc95335e68
vinay8309963 <107857409+vinay8309963@users.noreply.github.com>
2023-11-02 09:14:40 +0530

    Task3

--- app2.js
+++ app2.js
@@ -3,21 +3,16 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin.js');
+const shopRoutes = require('./routes/shop.js');
+
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use('/add-product',(req, res, next) => {
-    
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add Product</button></form>');
-});
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.post('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/');
-})
-
-app.use('/',(req, res, next) => {
-    
-    res.send('<h1>Hello from Express!</h1>');
+app.use((req, res, next)=> {
+    res.status(404).send('<h1>Page not Found</h1>');
 });
 
 app.listen(3000);
