a32d87f64fd48791809eb6e7c85067240e19db35
vinay8309963 <107857409+vinay8309963@users.noreply.github.com>
2023-11-02 20:32:00 +0530

    Task6

--- app2.js
+++ app2.js
@@ -1,20 +1,21 @@
-const http = require('http');
+const path = require('path');
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("In the another middleware");
-    next(); //Allows the request to continue to the next middleware in time.
+const adminRoutes = require('./routes/admin.js');
+const shopRoutes = require('./routes/shop.js');
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next)=> {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
-app.use((req, res, next) => {
-    console.log("In the another middleware");
-    res.send('<h1>Hello from Express!</h1>');
-});
-
-const server = http.createServer(app);
-
-server.listen(4000);
-
+app.listen(3000);
