b048445e1571d85bf743e60ec8483c4728603b6e
vinay8309963 <107857409+vinay8309963@users.noreply.github.com>
2023-11-02 09:12:48 +0530

    Task2

--- app2.js
+++ app2.js
@@ -1,20 +1,23 @@
-const http = require('http');
-
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("In the another middleware");
-    next(); //Allows the request to continue to the next middleware in time.
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/add-product',(req, res, next) => {
+    
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Add Product</button></form>');
 });
 
-app.use((req, res, next) => {
-    console.log("In the another middleware");
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+})
+
+app.use('/',(req, res, next) => {
+    
     res.send('<h1>Hello from Express!</h1>');
 });
 
-const server = http.createServer(app);
-
-server.listen(4000);
-
+app.listen(3000);
